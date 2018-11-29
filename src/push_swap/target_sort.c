96e77a5e26e64c1abef02e6473880a1d57c5759c
habdel-i <hassaineabdelillah@gmail.com>
2018-11-28 19:44:24 -0800

    waw

--- src/push_swap/target_sort.c
+++ src/push_swap/target_sort.c
@@ -1,15 +1,40 @@
 #include "libft.h"
 #include "push_swap.h"
 
+static int	get_pos(t_bundle *bundle, int index)
+{
+	t_numlist	*tmp;
+	int			i;
+
+	tmp = bundle->list_a;
+	i = 1;
+	while (tmp && tmp->index != index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
 void		target_sort(t_bundle *bundle)
 {
 	int			min;
+	int			pos;
 
 	min = 1;
 	while (!is_numlist_sorted(bundle->list_a))
 	{
-		while (bundle->list_a->index != min)
-			rra(bundle, 1);
+		pos = get_pos(bundle, min);
+		if (pos > bundle->list_a_length / 2)
+		{
+			while (bundle->list_a->index != min)
+				rra(bundle, 1);
+		}
+		else
+		{
+			while (bundle->list_a->index != min)
+				ra(bundle, 1);
+		}
 		pb(bundle, 1);
 		min++;
 	}
